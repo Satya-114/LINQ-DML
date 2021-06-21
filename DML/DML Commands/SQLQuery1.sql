4f1c3fff4795585c701ada5019ad73f9a19e99a5
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-21 08:06:58 +0530

    Add files via upload

---
+++ DML/DML Commands/SQLQuery1.sql
@@ -0,0 +1,7 @@
+insert Student values (101, 'joseph','plkl')
+insert Student (stuid,sName) values(102 , 'kv')
+select * from Student
+update  Student set slocation = 'nsp' where stuid = 102
+update student set slocation= 'nsp'
+delete from Student where stuid = 102
+delete from Student
